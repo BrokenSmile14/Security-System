66f47de943a9325798e8ef06dc29fa183c7096d7
Oleksandr-Dudnyk <108799411+Oleksandr-Dudnyk@users.noreply.github.com>
2023-03-23 16:10:29 +0200

    init blazor

---
+++ AlarmApp/Controllers/AlarmController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlarmApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlarmController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
